eaa0a6f98a51c85af55f46fd9e0e245a7bce0c5f
Samuel Attard <samuel.r.attard@gmail.com>
2016-01-26 20:36:31 +1300

    Update the roll / pitch movement calculations

--- src/index.js
+++ src/index.js
@@ -1,46 +1,43 @@
 const drone = window.drone;
 
+const NEUTRAL_PITCH = -25
+const NEUTRAL_ROLL = -90;
+
 const GRACE_ANGLE = 20;
 const AFFECT_ANGLE = 40;
 const SPEED_RATIO = 12;
 
+let socket;
+
 const connect = () => {
-  const socket = new WebSocket('ws://192.168.1.10:1338');
+  socket = new WebSocket('ws://192.168.1.14:1338');
   socket.onopen = () => {
     drone.takeoff();
   };
 
   socket.onmessage = (payload) => {
     const data = JSON.parse(payload.data);
-    let roll = data.orientation.roll;
-    const azi = data.orientation.azimuth;
-    if (azi < 180) {
-      roll = 90 + (90 - roll);
-    }
+    let pitch = data.pitch;
 
     // DEV: Forward / Backward motion
     let FBdelta = 0;
 
-    if (roll <= 60 - (GRACE_ANGLE / 2)) {
-      FBdelta = Math.abs((60 - (GRACE_ANGLE / 2)) - Math.max(60 - AFFECT_ANGLE, roll));
-    } else if (roll >= 60 + (GRACE_ANGLE / 2)) {
-      FBdelta = -1 * Math.abs((60 + (GRACE_ANGLE / 2)) - Math.min(60 + AFFECT_ANGLE, roll));
+    if (pitch <= NEUTRAL_PITCH - (GRACE_ANGLE / 2)) {
+      FBdelta = Math.abs((NEUTRAL_PITCH - (GRACE_ANGLE / 2)) - Math.max(NEUTRAL_PITCH - AFFECT_ANGLE, pitch));
+    } else if (pitch >= NEUTRAL_PITCH + (GRACE_ANGLE / 2)) {
+      FBdelta = -1 * Math.abs((NEUTRAL_PITCH + (GRACE_ANGLE / 2)) - Math.min(NEUTRAL_PITCH + AFFECT_ANGLE, pitch));
     }
     drone.front(FBdelta / SPEED_RATIO);
 
-    const pitch = data.orientation.pitch;
+    const roll = data.roll;
     // DEV: Rotating delta
     let Tdelta = 0;
-    if (pitch <= -1 * (GRACE_ANGLE / 2)) {
-      Tdelta = Math.max(-1 * AFFECT_ANGLE, pitch) + (GRACE_ANGLE / 2);
-    } else if (pitch >= (GRACE_ANGLE / 2)) {
-      Tdelta = Math.min(AFFECT_ANGLE, pitch) - (GRACE_ANGLE / 2);
+    if (roll <= NEUTRAL_ROLL - (GRACE_ANGLE / 2)) {
+      Tdelta = Math.abs((NEUTRAL_ROLL - (GRACE_ANGLE / 2)) - Math.max(NEUTRAL_ROLL - AFFECT_ANGLE, roll));
+    } else if (roll >= NEUTRAL_ROLL + (GRACE_ANGLE / 2)) {
+      Tdelta = -1 * Math.abs((NEUTRAL_ROLL + (GRACE_ANGLE / 2)) - Math.min(NEUTRAL_ROLL + AFFECT_ANGLE, roll));
     }
-    if (Tdelta <= 0) {
-      drone.counterClockwise(Tdelta / (AFFECT_ANGLE - GRACE_ANGLE / 2));
-    } else {
-      drone.clockwise(-1 * Tdelta / (AFFECT_ANGLE - GRACE_ANGLE / 2));
-    }
+    drone.clockwise(-1 * Tdelta / (AFFECT_ANGLE - GRACE_ANGLE / (2 * Math.max(1.2, FBdelta))));
   };
 
   socket.onerror = () => {
@@ -53,3 +50,28 @@ const connect = () => {
   };
 };
 connect();
+
+// DEV: Need to get these from the app somehow
+window.addEventListener('keydown', (e) => {
+  if (e.which === 38) {
+    drone.up(1);
+  } else if (e.which === 40) {
+    drone.down(1);
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.which === 38) {
+    drone.up(0);
+  } else if (e.which === 40) {
+    drone.down(0);
+  }
+});
+
+// Send the PNG back to the app
+drone._pngStream._emitPng = true;
+drone._pngStream.on('data', (pngString) => {
+  if (socket && socket.readyState === 1) {
+    socket.send(pngString);
+  }
+});
