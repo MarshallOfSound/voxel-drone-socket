8e48b37d76da752cbdbb2f5e6fcf42b0cf372655
Cole Diffin <cole.diffin@orionhealth.com>
2016-01-26 19:56:32 +1300

    Add dependencies
    what happened
    
    Fixes: #1

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const rename = require('gulp-rename');
 gulp.task('browserify', () => {
   gulp.src('src/index.js')
     .pipe(browserify())
-    .on('error', () => {})
+    .on('error', (e) => {console.error(e)})
     .pipe(rename('drone_controller.js'))
     .pipe(gulp.dest('base'));
 });
